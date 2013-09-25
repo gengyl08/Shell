4f96ff8e6a6c824899f99a5071be1ed0cc921543
Yilong Geng <gengyl08@stanford.edu>
2013-09-25 08:44:06 -0700

    initial set up

--- Shell/ShellViewController.h
+++ Shell/ShellViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ShellViewController : UIViewController
+<UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property BOOL newMedia;
+@property (weak, nonatomic) IBOutlet UIImageView *receiptPic;
+- (IBAction)takePicture:(id)sender;
+- (IBAction)submit:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *amount;
+@property (weak, nonatomic) IBOutlet UITextField *gallons;
+@property (weak, nonatomic) IBOutlet UITextField *fuelGrade;
 
 @end
--- Shell/ShellViewController.m
+++ Shell/ShellViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ShellViewController.h"
+#import <mobilecoreservices/UTCoreTypes.h>
 
 @interface ShellViewController ()
 
@@ -18,12 +19,84 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _amount.delegate = self;
+    _gallons.delegate = self;
+    _fuelGrade.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)takePicture:(id)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:
+         UIImagePickerControllerSourceTypeCamera])
+    {
+        UIImagePickerController *imagePicker =
+        [[UIImagePickerController alloc] init];
+        imagePicker.delegate = self;
+        imagePicker.sourceType =
+        UIImagePickerControllerSourceTypeCamera;
+        imagePicker.mediaTypes = @[(NSString *) kUTTypeImage];
+        imagePicker.allowsEditing = NO;
+        [self presentViewController:imagePicker
+                           animated:YES completion:nil];
+        _newMedia = YES;
+    }
+}
+
+- (IBAction)submit:(id)sender {
+}
+
+-(BOOL) textFieldShouldReturn:(UITextField *)textField{
+    
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    NSString *mediaType = info[UIImagePickerControllerMediaType];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    if (/*[mediaType isEqualToString:(NSString *)kUTTypeImage]*/1) {
+        UIImage *image = info[UIImagePickerControllerOriginalImage];
+        
+        _receiptPic.image = image;
+        if (_newMedia)
+            UIImageWriteToSavedPhotosAlbum(image,
+                                           self,
+                                           @selector(image:finishedSavingWithError:contextInfo:),
+                                           nil);
+    }
+    else if ([mediaType isEqualToString:(NSString *)kUTTypeMovie])
+    {
+        // Code here to support video if enabled
+    }
+}
+
+-(void)image:(UIImage *)image
+finishedSavingWithError:(NSError *)error
+ contextInfo:(void *)contextInfo
+{
+    if (error) {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle: @"Save failed"
+                              message: @"Failed to save image"
+                              delegate: nil
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
